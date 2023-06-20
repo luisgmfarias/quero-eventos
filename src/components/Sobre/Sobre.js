aee6fd9c37070df95beed37860abbf6b592f6c26
Luís Farias <luisgmfarias@gmail.com>
2023-06-20 19:25:51 -0300

    add Sobre page

---
+++ src/components/Sobre/Sobre.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { SobreStyles } from './styles';
+
+const Sobre = () => {
+  return (
+    <SobreStyles>
+      // Conteúdo do componente
+    </SobreStyles>
+  );
+};
+
+export default Sobre;
---
+++ src/components/Sobre/styles.js
@@ -0,0 +1,5 @@
+import styled from 'styled-components';
+
+export const SobreStyles = styled.div`
+  /* Estilos do componente */
+`;
