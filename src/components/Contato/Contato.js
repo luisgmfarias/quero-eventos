41568b333c5a8e4f97de2f0086481d37dfcce7a5
Luís Farias <luisgmfarias@gmail.com>
2023-06-20 19:25:07 -0300

    add contato page

---
+++ src/components/Contato/Contato.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { ContatoStyles } from './styles';
+
+const Contato = () => {
+  return (
+    <ContatoStyles>
+      // Conteúdo do componente
+    </ContatoStyles>
+  );
+};
+
+export default Contato;
---
+++ src/components/Contato/styles.js
@@ -0,0 +1,5 @@
+import styled from 'styled-components';
+
+export const ContatoStyles = styled.div`
+  /* Estilos do componente */
+`;
