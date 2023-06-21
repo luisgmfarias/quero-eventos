9fc5cf6a7c6053535ccff079842a1e645accc600
Luís Farias <luisgmfarias@gmail.com>
2023-06-20 21:04:13 -0300

    add global style

---
+++ src/GlobalStyles.js
@@ -0,0 +1,12 @@
+// GlobalStyles.js
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyles = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+    font-family: Quicksand, arial, sans-serif;
+  }
+`;
+
+export default GlobalStyles;
