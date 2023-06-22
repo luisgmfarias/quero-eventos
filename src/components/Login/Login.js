680679d4d98925ca053cb1c6233a099ac49c8d71
Luís Farias <luisgmfarias@gmail.com>
2023-06-22 07:12:38 -0300

    Login feature

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,11 +1,63 @@
-import React from 'react';
-import { CardHeader, CardHeading, CardBody, CardFieldset, CardButton, CardInput, CardIcon, CardLink } from './styles';
-import Card from '../Card/Card';
-import { useState } from 'react';
+import React from "react";
+import {
+  CardHeader,
+  CardHeading,
+  CardBody,
+  CardFieldset,
+  CardButton,
+  CardInput,
+  CardLink,
+} from "./styles";
+import Card from "../Card/Card";
+import { useState } from "react";
+import { login } from "../../services/auth";
+import { cadastrar } from "../../services/auth";
 
 const Login = () => {
+  const [isLogin, setIsLogin] = useState(false);
+  const [user, setUser] = useState("");
+  const [senha, setSenha] = useState("");
 
-  const [isLogin, setisLogin] = useState(false)
+  const handleLogin = async () => {
+    try {
+      const dadosLogin = {
+        user,
+        senha,
+        // Adicione outros campos do formulário de cadastro
+      };
+      const response = await login(dadosLogin);
+
+      localStorage.setItem("token", response.token);
+
+      setUser("");
+      setSenha("");
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleCadastro = async () => {
+    try {
+      const dadosCadastro = {
+        user,
+        senha,
+        // Adicione outros campos do formulário de cadastro
+      };
+
+      await cadastrar(dadosCadastro);
+      setUser("");
+      setSenha("");
+
+      // Exibir uma mensagem de sucesso ou redirecionar para outra página
+    } catch (error) {
+      // Lógica para tratar o erro de cadastro (exibição de mensagem de erro, etc.)
+      console.error(error);
+    }
+  };
+
+  const handleToggleLogin = () => {
+    setIsLogin(!isLogin);
+  };
 
   return (
     <Card>
@@ -16,29 +68,38 @@ const Login = () => {
       <CardBody>
         <CardFieldset>
           <CardInput
-            placeholder='Username'
-            type='text'
+            placeholder="Username"
+            type="text"
+            value={user}
+            onChange={(e) => setUser(e.target.value)}
             required
           />
         </CardFieldset>
 
         <CardFieldset>
           <CardInput
-            placeholder='Password'
-            type='password'
+            placeholder="Password"
+            type="password"
+            value={senha}
+            onChange={(e) => setSenha(e.target.value)}
             required
           />
         </CardFieldset>
 
         <CardFieldset>
-          <CardButton type='button'>{isLogin ? "Entrar" : "Cadastrar"}</CardButton>
+          <CardButton
+            type="button"
+            onClick={isLogin ? handleLogin : handleCadastro}
+          >
+            {isLogin ? "Entrar" : "Cadastrar"}
+          </CardButton>
         </CardFieldset>
-        {!isLogin &&
-          <CardFieldset>
-            <CardLink onClick={() => setisLogin(true)}>I already have an account</CardLink>
-          </CardFieldset>
-        }
 
+        <CardFieldset>
+          <CardLink onClick={handleToggleLogin}>
+            {!isLogin ? "Opa, eu já tenho uma conta!" : " Primeira vez?"}
+          </CardLink>
+        </CardFieldset>
       </CardBody>
     </Card>
   );
---
+++ src/services/auth.js
@@ -0,0 +1,42 @@
+export const login = async (dadosLogin) => {
+  try {
+    const response = await fetch("http://localhost:3001/api/auth/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(dadosLogin),
+    });
+
+    if (response.ok) {
+      const data = await response.json();
+      const token = data.token;
+
+      return { token };
+    } else {
+      throw new Error("Erro ao fazer login");
+    }
+  } catch (error) {
+    throw new Error(error.message);
+  }
+};
+
+export const cadastrar = async (dadosCadastro) => {
+  try {
+    const response = await fetch("http://localhost:3001/api/auth/cadastro", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(dadosCadastro),
+    });
+
+    if (response.ok) {
+      return await response.json();
+    } else {
+      throw new Error("Erro ao cadastrar");
+    }
+  } catch (error) {
+    throw new Error(error.message);
+  }
+};
