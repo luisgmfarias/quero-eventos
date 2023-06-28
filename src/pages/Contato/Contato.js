7045a315c12e5a4a384244534e18d20cf4530073
Luís Farias <luisgmfarias@gmail.com>
2023-06-28 01:17:56 -0300

    centering div

--- src/pages/Contato/Contato.js
+++ src/pages/Contato/Contato.js
@@ -1,10 +1,15 @@
-import React from 'react';
-import { ContatoStyles } from './styles';
+import React from "react";
+import { ContatoStyles } from "./styles";
+import { Link } from "react-router-dom";
 
 const Contato = () => {
   return (
     <ContatoStyles>
-      
+      <h1>Entre em contato</h1>
+      <p>
+        Você pode entrar em contato com o desenvolvedor por e-mail:
+      </p>
+      <Link href="mailto:luisgmfarias@gmail.com">Enviar e-mail</Link>
     </ContatoStyles>
   );
 };
--- src/pages/Contato/styles.js
+++ src/pages/Contato/styles.js
@@ -1,5 +1,8 @@
 import styled from 'styled-components';
 
 export const ContatoStyles = styled.div`
-  /* Estilos do componente */
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
 `;
